604f4d831276de9416f8b52d0d751d420b6007d8
Teodora Vodopija <48692987+teodoraa2019@users.noreply.github.com>
2020-05-04 08:37:28 +0200

    ..

---
+++ Assets/Neprijatelj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neprijatelj : MonoBehaviour {
+
+	public float brzina = 1f;
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction * brzina * Time.deltaTime;
+	}
+}
