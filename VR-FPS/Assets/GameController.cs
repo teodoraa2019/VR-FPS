499e3071c50c076a3e535bd7265cd8c626affa87
Teodora Vodopija <48692987+teodoraa2019@users.noreply.github.com>
2020-05-04 09:29:10 +0200

    ..

---
+++ VR-FPS/Assets/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Camera gameCamera;
+	public GameObject metakPrefab;
+	public GameObject neprijateljPrefab;
+	private float vrijemePucnja = 0;
+	private float vrijemeMirovanjaPucnja = 0;
+	private float NeprijateljSpawningTimer = 0;
+	public float NeprijateljSpawningMirovanje = 1f;
+	public float NeprijateljSpawningDistance = 7f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider)  {
+		if (Collider.tag == "Neprijatelj") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		vrijemePucnja -= Time.deltaTime;
+		NeprijateljSpawningTimer -= Time.deltaTime;
+
+		if (NeprijateljSpawningTimer <= 0f) {
+			NeprijateljSpawningTimer = NeprijateljSpawningMirovanje;
+
+			GameObject enemyObject = Instantiate (neprijateljPrefab);
+
+			float randomAngle = Random.Range (0, Mathf.PI * 2);
+			enemyObject.transform.position = new Vector3(gameCamera.transform.position.x + Mathf.Cos(randomAngle) * NeprijateljSpawningDistance, 0, 
+				gameCamera.transform.position.z + Mathf.Sin(randomAngle) * NeprijateljSpawningDistance);
+
+			Neprijatelj neprijatelj = enemyObject.GetComponent<Neprijatelj>();
+			neprijatelj.direction = (gameCamera.transform.ImagePosition - neprijatelj.transform.position).normalized;
+			neprijatelj.transform.LookAt(Vector3.zero);
+		}
+
+		RaycastHit hit;
+
+		if(Physics.Raycast(gameCamera.transform.position, gameCamera.transform.forward, out hit)) {
+			if (hit.transform.tag == "Neprijatelj" && vrijemePucnja <= 0f) {
+				vrijemePucnja = vrijemeMirovanjaPucnja = 0;
+
+				GameObject metakObject = Instantiate (metakPrefab);
+				metakObject.transform.position = gameCamera.transform.position;
+
+				Metak metak = metakObject.GetComponent<Metak> ();
+				metak.direction = gameCamera.transform.forward;
+			}
+		}
+	}
+}
---
+++ VR-FPS/Assets/Metak.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metak : MonoBehaviour {
+
+	public float brzina = 1f;
+	public Vector3 direction;
+	private float zivotnivijek = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction * brzina * Time.deltaTime;
+
+		zivotnivijek -= Time.deltaTime;
+		if(zivotnivijek <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Neprijatelj") {
+			Destroy (collider.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
